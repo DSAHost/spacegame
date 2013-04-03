13e9bf0c9922ae7569e13a4080ca39130a621072
Guest User <Guest@US-Science-02-MacBook-09.local>
2013-04-03 12:14:16 -0400

    Risk Fix

--- py/risk_combat.py
+++ py/risk_combat.py
@@ -1,8 +1,10 @@
 def combat(attacking_troops, defending_troops):
     attack = [random.randint(1,6)]
 	if attacking_troops >= 2:
+	if attacking_troops > 2:
 		attack.append(random.randint(1,6))
 	if attacking_troops >= 3:
+	if attacking_troops > 3:
 		attack.append(random.randint(1,6))
 	attack.sort()
 	defense = [random.randint(1,6)]

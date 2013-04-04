2333c0198e2f7225917f49f094bfcfad80b5a2ff
Andrew Schettino <andrewschettino@FAC-Lab-AliMac-02.local>
2013-04-04 10:33:59 -0400

    attk database

---
+++ AttackDatabase.py
@@ -0,0 +1,26 @@
+import webapp2
+import jinja2
+import os
+from google.appengine.ext import ndb
+import hashlib
+import hmac
+import logging
+import json
+from google.appengine.api import memcache
+
+class Attack(ndb.Model):
+	attacker_key=ndb.StringProperty(required=True)
+	defender_key=ndb.StringProperty(required=True)
+	units=ndb.IntegerProperty(required=True)
+	return_time=ndb.IntegerProperty(required=True)
+
+def attacks(update=False):
+	key="attacks"
+	attks=memcache.get(key)
+	if attks is None or update:
+		logging.error("ATTACKS QUERY")
+		attks=ndb.gql("SELECT * FROM Attack")
+		attks=list(attks)
+		memcache.set(key,attks)
+	return attks
+	

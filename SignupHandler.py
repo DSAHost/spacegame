9f69a33f55234f25b86256058769cacd2a983fb6
Andrew Schettino <andrewschettino@FAC-Lab-AliMac-02.local>
2013-04-08 11:37:22 -0400

    changes

--- SignupHandler.py
+++ SignupHandler.py
@@ -1,5 +1,19 @@
 from Handler import *
 
 class SignupHandler(Handler):
+	def render_front(self,username,usererror,password,passerror,error):
+		self.render("signup.html",username=username, usererror=usererror, password=password, passerror=passerror, error=error)
+
 	def get(self):
-		pass
+		self.render_front()
+
+	def post(self):
+		username=self.request.get('username')
+		password=self.request.get('password')
+		verify=self.request.get('verify')
+		email=self.request.get('email')
+		verifyerror=""
+		usererror=""
+		passerror=""
+		emailerror=""
+		accs=users()

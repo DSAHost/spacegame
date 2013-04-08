068735891519ab7087888806733f9e1eb6f8e9c6
Andrew Schettino <andrewschettino@FAC-Lab-AliMac-02.local>
2013-04-08 15:36:50 -0400

    login

--- LoginHandler.py
+++ LoginHandler.py
@@ -1,31 +1,4 @@
-import webapp2
-import jinja2
-import os
-from google.appengine.ext import ndb
-import logging
-import hmac
-import hashlib
-import json
-from time import strftime
-from google.appengine.api import memcache
-from datetime import *
-from UserDatabase import *
-from ResourceDatabase import *
 from Handler import *
-
-template_dir =os.path.join(os.path.dirname(__file__),'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),autoescape=True)
-
-class Handler(webapp2.RequestHandler):
-	def write(self,*a,**kw):
-		self.response.out.write(*a,**kw)
-
-	def render_str(self,template,**params):
-		t=jinja_env.get_template(template)
-		return t.render(params)
-
-	def render(self,template, **kw):
-		self.write(self.render_str(template,**kw))
 
 class LoginHandler(Handler):
 	def render_front(self,username="",error=""):

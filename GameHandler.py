234c68d157962d9958b4199ff66270b36c94609f
Ben Hirsch <benhirsch@FAC-Lab-i5-iMac-04.local>
2013-04-09 12:04:59 -0400

    I know!

--- GameHandler.py
+++ GameHandler.py
@@ -4,9 +4,4 @@ class GameHandler(Handler):
 	def render_front(self, username, currency, units):
 		self.render("gamebase.html",username=username,currency=currency, units=units)
 	def get(self):
-<<<<<<< HEAD
-		
-		self.render_front()
-=======
 		self.render("gamefront.html")
->>>>>>> lotsa stuff

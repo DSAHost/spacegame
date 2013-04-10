15e07720cb3e54bfb224ae4fa8dc0d263ae52f49
Ryan Christianson <ryanchristianson@FAC-Lab-AliMac-06.local>
2013-04-10 13:55:02 -0400

    Implemented AttackHandler

--- AttackHandler.py
+++ AttackHandler.py
@@ -3,4 +3,21 @@ from utils import *
 
 class AttackHandler(Handler):
 	def get(self):
-		pass
+		units = self.request.get('units')
+		action = self.request.get('action')
+		target = self.request.get('target')
+		attack_id = self.request.get('attack_id')
+
+		if units and target:
+			attack = Attack(attacker_key = self.user.key, defender_key = target.key, units = units)
+			attack.put()
+			return
+
+		if action and attack_id:
+			if action = 'attack':
+				defender = get_Resources(defender_key)
+				units, defender.home_units = combat(units, defender.home_units)
+
+			if action == 'retreat':
+				return_time = datetime().now + dattime.timedelta(minutes = return_time)
+				attack.return_time = return_time

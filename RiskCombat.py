037c5891de51682fd84385fc998d57a9945fb7ee
Ben Hirsch <benhirsch@FAC-Lab-i5-iMac-04.local>
2013-04-10 13:47:51 -0400

    ...

--- RiskCombat.py
+++ RiskCombat.py
@@ -1,5 +1,5 @@
 def combat(attacking_troops, defending_troops):
-    attack = [random.randint(1,6)]
+	attack = [random.randint(1,6)]
 	if attacking_troops >= 3:
 		attack.append(random.randint(1,6))
 	if attacking_troops >= 9:
@@ -15,7 +15,7 @@ def combat(attacking_troops, defending_troops):
 	defense.sort()
 	i=0
 	while i<len(attack) and i<len(defense):
-		if attack[i] > defense[i]
+		if attack[i] > defense[i]:
 			defending_troops-=1
 		else:
 			attacking_troops-=1

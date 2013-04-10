7ccc41d43d007e3b6d2010c8cc3da658a5776762
Ben Hirsch <benhirsch@FAC-Lab-i5-iMac-04.local>
2013-04-10 09:59:47 -0400

    ...

--- GameHandler.py
+++ GameHandler.py
@@ -1,12 +1,14 @@
 from Handler import *
 
 class GameHandler(Handler):
-	def render_front(self, username, currency, units):
-		self.render("gamebase.html",username=username,currency=currency, units=units)
+	# def render_front(self, username, currency, units):
+	# 	self.render("gamebase.html",username=username,currency=currency, units=units)
+	# def get(self):
+	# 	if self.user:
+	# 		username=self.user.username
+	# 		resources=ResourceDatabase.getResources(self.user.resource_key)
+	# 		self.render_front(username,resources[0],resources[1])
+	# 	else:
+	# 		self.redirect('/login')
 	def get(self):
-		if self.user:
-			username=self.user.username
-			resources=ResourceDatabase.getResources(self.user.resource_key)
-			self.render_front(username,resources[0],resources[1])
-		else:
-			self.redirect('/login')
+		self.render("gamefront.html")

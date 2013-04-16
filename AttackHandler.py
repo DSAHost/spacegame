b31f9fd5ceba1f069cc8f93ffbef835d4efc64ca
Ryan Christianson <ryanchristianson@FAC-Lab-AliMac-06.local>
2013-04-16 10:23:45 -0400

    attack

--- AttackHandler.py
+++ AttackHandler.py
@@ -26,15 +26,15 @@ class AttackHandler(Handler):
 		if units and target:
 			attack = Attack(attacker_key = self.user.key, defender_key = target.key, units = units)
 			attack.put()
-			return units
+			return
 
 		if action and attack_id:
 			attack = attacks()[attack_id]
 			if action == 'attack':
 				defender = get_Resources(attack.defender_key)
 				units, defender.home_units = combat(attack.units, defender.home_units)
-				return units, defender.home_units
+				return
 				
 			if action == 'retreat':
 				attack.return_time = datetime().now + dattime.timedelta(minutes = return_time)
-				return units, defender.home_units
+				return

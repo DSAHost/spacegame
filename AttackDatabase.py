e2fa68a827d23e6c8ed4466cfc1e6a012c3474d3
Andrew Schettino <andrewschettino@FAC-Lab-AliMac-02.local>
2013-04-16 15:08:40 -0400

    g

--- AttackDatabase.py
+++ AttackDatabase.py
@@ -1,8 +1,9 @@
 from Handler import *
+from UserDatabase import *
 
 class Attack(ndb.Model):
-	attacker_key=ndb.StringProperty(required=True)
-	defender_key=ndb.StringProperty(required=True)
+	attacker_key=ndb.KeyProperty(required=True)
+	defender_key=ndb.KeyProperty(required=True)
 	num_troops=ndb.IntegerProperty(required=True)
 	time_fought=ndb.DateTimeProperty(auto_now_add=True)
 	return_time=ndb.IntegerProperty(required=True)
@@ -17,7 +18,7 @@ def attacks(update=False):
 		memcache.set(key,attks)
 	return attks
 
-def is_Finished(key):
+def isFinished(key):
 	attk=key.get()
 	dif=(datetime.datetime.now()-attk.time_fought).total_seconds()
 	if dif>attk.return_time:
@@ -25,3 +26,11 @@ def is_Finished(key):
 		return True
 	return False
 
+def newAttack(attkerkey,defender,troops,time):
+	accs=users()
+	dkey=None
+	for i in accs:
+		if i.username==defender:
+			dkey=i.key
+	attk=Attack(attacker_key=attkerkey, defender_key=dkey, num_troops=troops, return_time=time)
+	attk.put()
--- AttackHandler.py
+++ AttackHandler.py
@@ -1,7 +1,7 @@
 from Handler import *
 from utils import *
 from UserDatabase import *
-
+from AttackDatabase import *
 class AttackHandler(Handler):
 	def render_front(self,username,currency,units,available_targets=None,attack_id=None):
 		if attack_id:
@@ -14,7 +14,8 @@ class AttackHandler(Handler):
 		usrs=list(usrs)
 		fusrs=[]
 		for i in usrs:
-			fusrs.append(i.username)
+			if i != self.user:
+				fusrs.append(i.username)
 		if self.user:
 	 		username=self.user.username
 	 		resources=ResourceDatabase.getResources(self.user.resource_key)
@@ -23,14 +24,13 @@ class AttackHandler(Handler):
 	 		self.redirect('/login')
 		
 	def post(self):
-		units = self.request.get('num_troops')
-		target = self.request.get('target')
+		units = int(self.request.get('num_troops'))
+		target = str(self.request.get('target'))  #username
 		action = self.request.get('action')
 		attack_id = self.request.get('attack_id')
 
 		if units and target:
-			# attack = new Attack(attacker_key=self.user.key, defender_key=target.key, units=units)
-			attack.put()
+			newAttack(self.user.key,target, units, int(1000))
 			return
 
 		if action and attack_id:
@@ -41,5 +41,5 @@ class AttackHandler(Handler):
 				return
 				
 			if action == 'retreat':
-				attack.return_time = datetime().now + dattime.timedelta(minutes = return_time)
+				attack.return_time = datetime().now + datetime.timedelta(minutes = return_time)
 				return

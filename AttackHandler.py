b1725e5abcfa087f435db0b37753b94634ee4e0e
Arschettino <arschettino@gmail.com>
2013-04-23 21:54:14 -0300

    Update AttackHandler.py

--- AttackHandler.py
+++ AttackHandler.py
@@ -49,15 +49,16 @@ class AttackHandler(Handler):
 					self.user.addCombatUnits(-1*troops)
 					om=troops
 					myunits=troops
-					theirunits=i.resources.home_units
+					theirunits=i.getHomeUnits()
 					ot=theirunits
 					myunits,theirunits=RiskCombat.combat(myunits,theirunits)
 					i.addCombatUnits(-1*(ot-theirunits))
 					self.user.newAttack(i.username,myunits,60)
-					spoils=RiskCombat.spoilsOfWar(om-myunits,ot-theirunits,i.resources.currency)
+					spoils=RiskCombat.spoilsOfWar(om-myunits,ot-theirunits,i.getResources()[0])
 					spoils=int(spoils)
 					self.user.addCurrency(spoils)
 					i.addCurrency(-1*spoils)
 					self.user.newMessage("You attacked %s." % i.username, "You lost %d troops and plundered %d currency." % (om-myunits,spoils))
 					i.newMessage("You were attacked by %s." % self.user.username, "You lost %d troops and %d currency." % (ot-theirunits,spoils))
+					return
 		self.redirect('/game')

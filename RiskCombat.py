bb2b3be79970b94f415a5c1090e7f454b089c23f
Yellow4 <tgillani@FAC-Lab-AliMac-03.local>
2013-04-25 08:53:53 -0400

    [ADD] new combat module
    new!

--- RiskCombat.py
+++ RiskCombat.py
@@ -1,6 +1,6 @@
 import random
 
-def combat(attacking_troops, defending_troops):
+def old_combat(attacking_troops, defending_troops):
 	attack = [random.randint(1,6)]
 	if attacking_troops >= 3:
 		attack.append(random.randint(1,6))
@@ -23,6 +23,16 @@ def combat(attacking_troops, defending_troops):
 			attacking_troops-=1
 		i+=1
 	return (attacking_troops,defending_troops)
+
+def combat(attacking_troops,defending_troops):
+	atk_f=0.0+attacking_troops
+	attack=random.randint((int(atk_f*.7),int(atk_f*1.1)
+	def_f=0.0+defending_troops
+	defense=random.randint(int(def_f*.85),int(def_f))
+	diff=defense-attack
+	atk_r=attacking_troops-int(diff*1.1)
+	def_r=defending_troops-int(diff*.95)
+	return (atk_r,def_r)
 
 def spoilsOfWar(attackers_dead,defenders_dead,defender_currency):
 	if defenders_dead>(1.5*attackers_dead):

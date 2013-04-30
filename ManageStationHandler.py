8c92779c9c2ebf6d050ae6be43be69067d55b3ef
Andrew Schettino <andrewschettino@FAC-Lab-AliMac-02.local>
2013-04-30 11:49:45 -0400

    e

--- ManageStationHandler.py
+++ ManageStationHandler.py
@@ -2,10 +2,26 @@ from Handler import *
 from utils import *
 
 class ManageStationHandler(Handler):
-	def render_front(self):
-		self.render("managestation.html")
+	def render_front(self,error=""):
+		self.render("managestation.html",error=error)
 	def get(self):
 	 	if self.user:
 	 		self.render_front()
 	 	else:
 	 		self.redirect('/login')
+
+	def post(self):
+		buying=0
+	 	try:
+	 		buying=int(self.request.get('units'))
+	 	except ValueError:
+	 		self.render_front(error="You must enter a valid number.")
+	 		return
+	 	resources=self.user.getResources()[0]
+	 	cost=buying*35
+	 	if buying<=0:
+	 		self.render_front(error="You must enter a valid number.")
+	 		return
+	 	if cost>resources:
+	 		self.render_front(error="You do not have enough credits.")
+	 		return
